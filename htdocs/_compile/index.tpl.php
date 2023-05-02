5fcfff1c8f305ede249bb149e02c0132d55dd2cc
Daehyun Kyung <cokolavel@gmail.com>
2023-05-02 23:02:14 +0900

    템플릿 언더바 기본 세팅

---
+++ htdocs/_compile/index.tpl.php
@@ -0,0 +1,12 @@
+<?php /* Template_ 2.2.8 2023/05/01 10:03:54 C:\Users\PC\PhpstormProjects\PHP-template_\htdocs\_template\index.tpl 000000268 */ ?>
+<html>
+<head>
+
+<title><?php echo $TPL_VAR["title"]?></title>
+</head>
+<body>
+<?php echo $TPL_VAR["content"]?>
+
+</body>
+
+</html>
